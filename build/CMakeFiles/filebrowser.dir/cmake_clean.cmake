600d94ab43c9159ffa42f244b09bd94ae3147063
vitaly <8vito8@rambler.ru>
2015-02-16 23:55:58 +0300

    remove permission-denied warning

---
+++ build/CMakeFiles/filebrowser.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/filebrowser.dir/filebrowser_gtkmm/directorychooser.cpp.o"
+  "CMakeFiles/filebrowser.dir/filebrowser_gtkmm/directoryscanner.cpp.o"
+  "CMakeFiles/filebrowser.dir/filebrowser_gtkmm/mainwindow.cpp.o"
+  "CMakeFiles/filebrowser.dir/filebrowser_gtkmm/main.cpp.o"
+  "filebrowser.pdb"
+  "filebrowser"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/filebrowser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
